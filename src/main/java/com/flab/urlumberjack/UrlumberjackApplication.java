299442dd018bae28c73eef8a34a1740dab4d1b86
JeaPill.Yoon <44865986+gr2nsky@users.noreply.github.com>
2023-05-02 18:45:38 +0900

    init project

---
+++ src/main/java/com/flab/urlumberjack/UrlumberjackApplication.java
@@ -0,0 +1,13 @@
+package com.flab.urlumberjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlumberjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlumberjackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/urlumberjack/UrlumberjackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.urlumberjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlumberjackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
